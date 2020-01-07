3a51e6c7930640ab6f746b23e198143c64f99548
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-01-08 01:38:27 +0900

    add topPage

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
 <template>
-    <p>This page is the top</p>
+    <section>
+        <h5 class="title">Welocome To Your Arrival</h5>
+        <p class="message">This Site is For Well Book Reader</p>
+        <p class="message">{{ message }}</p>
+        <div class="button-wrap">
+            <button class="button is-primary" @click="enterSite">YES</button> <button class="button is-primary" @click="notEnterSite">NO</button>
+        </div>
+    </section>
 </template>
+<script>
+export default {
+    data () {
+        return {
+            message: "Do You Use This Site ?"
+        }
+    },
+    methods: {
+        enterSite() {
+            this.$router.push("/books")
+        },
+        notEnterSite() {
+            this.message = "Thanks See You Again !"
+        }
+    }
+}
+</script>
+<style scoped>
+    .title{
+        font-size: 32px;
+        font-weight: bold;
+    }
+    .message{
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .button-wrap{
+        margin: 5% auto;
+    }
+</style>
